9f754ba88403af2642afb6b87d9eaeb879188f38
liuzhenyu <951689833@qq.com>
2021-05-25 16:07:59 +0800

    '练习到model'

--- config/config.js
+++ config/config.js
@@ -2,10 +2,38 @@ export default {
     singular: true,
     dva: {},
     antd: {},
+    // plugins: [
+    //     ['umi-plugin-react', {
+    //         antd: true
+    //     }]],
     routes: [
         {
             path: '/',
-            component: './HelloWorld',
+            component: '../layout',
+            routes: [
+                {
+                    path: '/helloworld',
+                    component: './HelloWorld'
+                },
+                {
+                    path: 'puzzlecards',
+                    component: './puzzlecards'
+                },
+                {
+                    path: '/dashboard',
+                    routes: [
+                        { path: '/dashboard/analysis',component: 'Dashboard/Analysis'},
+                        { path: '/dashboard/monitor',component: 'Dashboard/Monitor'},
+                        { path: '/dashboard/workplace',component: 'Dashboard/Workplace'},
+                    ]
+                }
+            ]
         }
-    ]
+    ],
+    proxy: {
+        '/dev': {
+            target: 'http://localhost:3005',
+            changeOrigin: true,
+            },
+  },
 };
--- src/page/HelloWorld.js
+++ src/page/HelloWorld.js
@@ -1,3 +1,23 @@
+import { Card } from 'antd'
+
 export default () => {
-    return <div>hello world</div>;
+    const style = {
+        width: '400px',
+        margin: '30px',
+        boxShadow: '0 4px 8px 0 rgb(0,0,0,0.2)',
+        border: '1px solid #e8e8e8',
+    };
+    return (
+        <Card style={style} actions={[<a>操作一</a>, <a>操作二</a>]}>
+            <Card.Meta
+            avatar={<img
+            alt=""
+            style={{width:'64px',height: '64px',borderRadius: '32px'}}
+            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+            />}
+            title="美女模特"
+            description="美女模特在线发牌"
+            />
+        </Card>
+    )
 }
